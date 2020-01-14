1cc576c628bc7b3ec73ce9503311450d698cfe5b
eannoni <annoni@chapman.edu>
2020-01-14 20:52:04 +0000

    first file added

---
+++ github_test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+  string name;
+
+  cout << "This is a test file for GitHub!" << endl;
+
+  cout << "What is your name?" << endl;
+  cin >> name;
+  cout << "Welcome, " << name << "!" << endl;
+
+  return 0;
+}
